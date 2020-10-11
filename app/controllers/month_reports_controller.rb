e311e42b2dd10f08063fcb6e641bc00263b7f0f0
marviorocha <marviorocha@marviorocha.com>
2020-10-11 19:27:31 -0300

    reports with 'new gem prawn'

--- app/controllers/month_reports_controller.rb
+++ app/controllers/month_reports_controller.rb
@@ -9,14 +9,7 @@ class MonthReportsController < ApplicationController
             @projection_debt = ProjectionDebt.new(Debt.find(params[:debt_id]), start_date + 1.month)
             @start_date = @projection_debt.start_date
             @future_transactions = @projection_debt.transaction_items    
-    
-            respond_to do |format|
-              format.pdf do
-                  render :pdf => 'relatorio'
-              end
-              
-            end
-              
+ 
   end
 
 end
--- app/models/projection_debt.rb
+++ app/models/projection_debt.rb
@@ -7,12 +7,13 @@ class ProjectionDebt
   attr_accessor :amortizations_count
   attr_accessor :transaction_items
   attr_accessor :final_outstanding_balance
+  
   def initialize(debt, start_date = signature_date)
     self.debt = debt
     self.start_date = start_date
     self.amortizations_count = start_date_to_amortizations_count
     self.transaction_items = build_transaction_items
-    #self.final_outstanding_balance = final_outstanding_balance
+    self.final_outstanding_balance = final_outstanding_balance
   end
   
   def build_transaction_items
@@ -26,10 +27,7 @@ class ProjectionDebt
           if self.start_date == signature_date
             self.balance_projection = debt.transaction_items.where.not(:confirmed => false).last.final_outstanding_balance
           else
-            
-            self.balance_projection = debt.transaction_items.where('date <= ?', self.start_date).last.final_outstanding_balance
-            
-            
+            self.balance_projection = debt.transaction_items.where('date <= ?', self.start_date).last.final_outstanding_balance           
           end
         else 
           self.balance_projection = result.last.final_outstanding_balance
