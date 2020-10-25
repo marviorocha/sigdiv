7a3122e2a1ba19201755798b2a5a40ade211d3ad
marviorocha <marviorocha@marviorocha.com>
2020-10-25 17:54:33 -0300

    Stap 1

--- app/helpers/currencies_helper.rb
+++ app/helpers/currencies_helper.rb
@@ -4,11 +4,24 @@ module CurrenciesHelper
 
     def name_to_currency(name)
         case name
-         when "USD"
+        when "USD"
              BancoCentral.last(:dolar)
-         when "EURO"
+        when "EURO"
              BancoCentral.last(:euro)
+        when "DOLAR_VENDA"
+            BancoCentral.last(:dolar_venda)
+        when "DOLAR_VENDA"
+            BancoCentral.last(:dolar_compra)
+        when "EURO VENDA"
+            BancoCentral.last(:euro_venda)
+        when "EURO COMPRA"
+            BancoCentral.last(:euro_compra)
+        when "IENE"
+            BancoCentral.last(:iene)
+        when "FRANCO SUIÇO"
+            BancoCentral.last(:franco_suico)
          else
+  
          end
         
     end
