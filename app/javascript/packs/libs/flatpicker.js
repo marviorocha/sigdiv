8de4a1eb1e67c4fb2e567f13feaefd4a14b0fbfe
marviorocha <marviorocha@marviorocha.com>
2020-11-03 19:26:55 -0300

    heroku deploy 30

--- app/javascript/packs/libs/flatpicker.js
+++ app/javascript/packs/libs/flatpicker.js
@@ -1,4 +1,4 @@
-import "flatpickr/dist/flatpickr.min.css";
+require("flatpickr/dist/themes/material_blue.css");
 import flatpickr from "flatpickr";
 import { Portuguese } from "flatpickr/dist/l10n/pt.js";
 
