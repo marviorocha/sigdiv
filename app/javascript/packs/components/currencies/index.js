3ff47b13b80e0a49577531d18e775d57c7bc2560
marviorocha <marviorocha@marviorocha.com>
2020-11-18 22:51:48 -0300

    Currencies

--- app/javascript/packs/components/currencies/index.js
+++ app/javascript/packs/components/currencies/index.js
@@ -7,13 +7,12 @@ const Currencies = () => {
   const [currencie, setCurrencie] = useState([]);
   const [bndes, setBndes] = useState([]);
   const [load, setLoad] = useState(true);
-  
+
   const token = "95e1a099-ced5-3b11-b6c4-7766151c9ac6";
 
   useEffect(() => {
-     
     const started = setInterval(() => {
-      setLoad(false)
+      setLoad(false);
       axios
         .get("/api/v1/currencies.json")
         .then((response) => {
@@ -21,91 +20,94 @@ const Currencies = () => {
         })
         .catch((error) => {
           console.log(error);
-        })
+        });
     }, 1000);
 
-  return () => {clearInterval(started)} 
-
+    return () => {
+      clearInterval(started);
+    };
   }, [currencie.length]);
 
- 
- 
- 
-    const currenciesPages = currencie.map((item) => {
-      const edit_item = "/currencies/" + item.attributes.id + "/edit";
-      const delete_item = "/currencies/" + item.attributes.id;
-      const dataStarted = dayjs(item.attributes.date_currency).format('YYYYMMDD') 
+  const currenciesPages = currencie.map((item) => {
+    const edit_item = "/currencies/" + item.attributes.id + "/edit";
+    const delete_item = "/currencies/" + item.attributes.id;
+    const dataStarted = dayjs(item.attributes.date_currency).format("YYYYMMDD");
 
-      axios
+    axios
       .get(
         `https://apis-gateway.bndes.gov.br/moedascontratuais/v1/servicoListaCotacoes?serie=${item.attributes.code}&dataInicio=${dataStarted}&dataFim=${dataStarted}&limite=1`,
         { headers: { Authorization: `Bearer ${token}` } }
       )
-        .then((response) => {
- 
-          axios.patch(`api/v1/currencies/${item.attributes.id}`, {
-          
-          last_currency: response.data.listaCotacaoMoeda[0]["valor"],
-          date_currency: item.attributes.date_currency,
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-
+      .then((response) => {
+        axios
+          .patch(`api/v1/currencies/${item.attributes.id}`, {
+            last_currency: response.data.listaCotacaoMoeda[0]["valor"],
+            date_currency: item.attributes.date_currency,
+          })
+          .catch((error) => {
+            console.log(error);
+          });
       })
       .catch((error) => {
         console.log(error);
-      })
+      });
 
-      return (
-        <Fragment>
-          <tr key={item.attributes.id}>
-            <td>{item.attributes.name}</td>
-            <td>{item.attributes.formula}</td>
-            <td>{item.attributes.description}</td>
-            <td>{item.attributes.last_currency}</td>
-            <td>
-              { item.attributes.date_currency ?  dayjs(item.attributes.date_currency).format('DD/MM/YYYY') : '' }
-            </td>
+    return (
+      <Fragment>
+        <tr key={item.attributes.id}>
+          <td>{item.attributes.name}</td>
+          <td>{item.attributes.formula}</td>
+          <td>{item.attributes.description}</td>
+          <td>{item.attributes.last_currency}</td>
+          <td>
+            {item.attributes.date_currency
+              ? dayjs(item.attributes.date_currency).format("DD/MM/YYYY")
+              : ""}
+          </td>
 
-            <td>
-              <a
-                data-toggle="modal"
-                data-target="#openModal"
-                data-remote="true"
-                href={delete_item}
-              >
-                <i title="Mostrar" className="fa fa-money fa-2x"></i>
-              </a>
-            </td>
-            <td>
-              <a
-                data-toggle="modal"
-                data-target="#openModal"
-                data-remote="true"
-                href={edit_item}
-              >
-                <i data-toggle="tooltip" title="Editar" className="fa fa-edit fa-2x"></i>
-              </a>
-            </td>
-            <td>
-              <a
-                data-confirm="Você tem certeza que deseja excluir esse item?"
-                data-toggle="modal"
-                data-target="#openModal"
-                data-remote="true"
-                data-method="delete"
-                href={delete_item}
-              >
-                <i title="Excluir" className="fa fa-trash fa-2x"></i>
-              </a>
-            </td>
-          </tr>
-        </Fragment>
-      );
-    });
-    const spinnel =  <i className="fa fa-spinner fa-pulse fa-3x fa-fw"></i> 
-    return <Fragment>{load ? spinnel : currenciesPages }</Fragment>;
+          <td>
+            <a
+              data-toggle="modal"
+              data-target="#openModal"
+              data-remote="true"
+              href={delete_item}
+            >
+              <i title="Mostrar" className="fa fa-money fa-2x"></i>
+            </a>
+          </td>
+          <td>
+            <a
+              data-toggle="modal"
+              data-target="#openModal"
+              data-remote="true"
+              href={edit_item}
+            >
+              <i
+                data-toggle="tooltip"
+                title="Editar"
+                className="fa fa-edit fa-2x"
+              ></i>
+            </a>
+          </td>
+          <td>
+            <a
+              data-confirm="Você tem certeza que deseja excluir esse item?"
+              data-toggle="modal"
+              data-target="#openModal"
+              data-remote="true"
+              data-method="delete"
+              href={delete_item}
+            >
+              <i title="Excluir" className="fa fa-trash fa-2x"></i>
+            </a>
+          </td>
+        </tr>
+      </Fragment>
+    );
+  });
+  const spinnel = <i className="fa fa-spinner fa-pulse fa-3x fa-fw"></i>;
+
+  return <Fragment>{load ? spinnel : currenciesPages}</Fragment>;
 };
 
 document.addEventListener("DOMContentLoaded", () => {
