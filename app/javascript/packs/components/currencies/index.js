f5e048c6ca953fbad989fc39e0a4ac6ceb4fcac5
marviorocha <marviorocha@marviorocha.com>
2020-11-19 12:33:02 -0300

    first refatory currency

--- app/javascript/packs/components/currencies/index.js
+++ app/javascript/packs/components/currencies/index.js
@@ -2,37 +2,41 @@ import React, { useState, useEffect, Fragment } from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
 import dayjs from "dayjs";
-$('[data-toggle="tooltip"]').tooltip();
+
 const Currencies = () => {
+  
   const [currencie, setCurrencie] = useState([]);
   const [bndes, setBndes] = useState([]);
   const [load, setLoad] = useState(true);
-
   const token = "95e1a099-ced5-3b11-b6c4-7766151c9ac6";
 
+  // Get All Currencie from API 
+  async function CurrencieAll() {
+   
+      let response = await axios.get('/api/v1/currencies.json');
+      setCurrencie(response.data.data);
+  }
+  
+  
+ 
   useEffect(() => {
+
     const started = setInterval(() => {
       setLoad(false);
-      axios
-        .get("/api/v1/currencies.json")
-        .then((response) => {
-          setCurrencie(response.data.data);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
+      CurrencieAll();
     }, 1000);
-
     return () => {
       clearInterval(started);
     };
   }, [currencie.length]);
 
-  const currenciesPages = currencie.map((item) => {
+    const currenciesPages = currencie.map((item) => {
     const edit_item = "/currencies/" + item.attributes.id + "/edit";
     const delete_item = "/currencies/" + item.attributes.id;
     const dataStarted = dayjs(item.attributes.date_currency).format("YYYYMMDD");
 
+    
+      
     axios
       .get(
         `https://apis-gateway.bndes.gov.br/moedascontratuais/v1/servicoListaCotacoes?serie=${item.attributes.code}&dataInicio=${dataStarted}&dataFim=${dataStarted}&limite=1`,
@@ -53,9 +57,9 @@ const Currencies = () => {
       });
 
     return (
-      <Fragment>
-        <tr key={item.attributes.id}>
-          <td>{item.attributes.name}</td>
+    
+        <tr>
+          <td key={item.attributes.id} >{item.attributes.name}</td>
           <td>{item.attributes.formula}</td>
           <td>{item.attributes.description}</td>
           <td>{item.attributes.last_currency}</td>
@@ -66,24 +70,27 @@ const Currencies = () => {
           </td>
 
           <td>
-            <a
+          <a
+             
               data-toggle="modal"
               data-target="#openModal"
               data-remote="true"
+              data-tooltip="tooltip"
               href={delete_item}
             >
-              <i title="Mostrar" className="fa fa-money fa-2x"></i>
+              <i    title="Mostrar" className="fa fa-money fa-2x"></i>
             </a>
           </td>
           <td>
-            <a
+          <a
+             
               data-toggle="modal"
               data-target="#openModal"
               data-remote="true"
               href={edit_item}
             >
               <i
-                data-toggle="tooltip"
+              
                 title="Editar"
                 className="fa fa-edit fa-2x"
               ></i>
@@ -102,10 +109,10 @@ const Currencies = () => {
             </a>
           </td>
         </tr>
-      </Fragment>
+       
     );
   });
-  const spinnel = <i className="fa fa-spinner fa-pulse fa-3x fa-fw"></i>;
+  const spinnel = <div className="center"><i className="fa fa-spinner fa-pulse fa-3x fa-fw"></i></div>;
 
   return <Fragment>{load ? spinnel : currenciesPages}</Fragment>;
 };
